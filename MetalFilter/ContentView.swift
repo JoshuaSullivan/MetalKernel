79bd326cf550a4d4c95058af9fd72d38d26e486c
Joshua Sullivan <joshua.sullivan@spok.com>
2020-06-24 14:15:35 -0500

    Initial Commit

---
+++ MetalFilter/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  MetalFilter
+//
+//  Created by Joshua.Sullivan on 6/24/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MetalFilter/MetalFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalFilterApp.swift
+//  MetalFilter
+//
+//  Created by Joshua.Sullivan on 6/24/20.
+//
+
+import SwiftUI
+
+@main
+struct MetalFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
